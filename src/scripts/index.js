8012f7a38db37e02208580fe55210b67da1cc813
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-24 00:20:10 +0700

    Update project + Issue occurs while fetching data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,15 @@
 import 'regenerator-runtime' /* for async await transpile */
 import '../styles/main.scss'
 import 'normalize.css'
+import axios from 'axios'
+
+async function main () {
+  try {
+    const response = await axios.get('https://api.jikan.moe/v4/anime?q=chainsaw&sfw')
+    console.log(response)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+main()
