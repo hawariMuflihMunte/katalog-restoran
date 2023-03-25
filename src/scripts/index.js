d18f64d4d4134089c8410478f1e10a229cdc3e27
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-26 00:34:03 +0700

    Reorganize & update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime' /* for async await transpile */
-import '../styles/main.scss'
 import 'normalize.css'
+
+import '../styles/main.scss'
 import DATA from '../data/DATA.json'
 
 async function main () {
