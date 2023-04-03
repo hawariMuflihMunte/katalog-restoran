981d97eb4b971f15f53c134db352c37838a2b2bc
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-03 12:50:32 +0700

    Create ARIA

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -3,6 +3,11 @@ class DishPlateCard extends HTMLElement {
     this._data = obj
   }
 
+  // eslint-disable-next-line accessor-pairs
+  set clickEvent (_event) {
+    this._clickEvent = _event
+  }
+
   get data () {
     return this._data
   }
@@ -15,20 +20,24 @@ class DishPlateCard extends HTMLElement {
     console.log(this._data)
 
     this.innerHTML = `
-      <a href="" class="card" id="${this._data.id}">
-        <div class="header">
-          <div class="pin">
+      <div class="card" aria-label="Card details">
+        <div class="header" aria-label="Card header">
+          <div class="pin" aria-label="City info">
               ${this._data.city}
           </div>
           <img src="${this._data.pictureId}" alt="${this._data.name}" loading="lazy">
         </div>
-        <div class="content">
-          <h4 class="rating">Rating ${this._data.rating}</h4>
-          <h3 class="title">${this._data.name}</h3>
-          <p class="description">${this._data.description}</p>
+        <div class="content" aria-label="Card Content">
+          <h3 class="rating" aria-label="Rating info">Rating ${this._data.rating}</h3>
+          <h4 class="title" aria-label="Card title">${this._data.name}</h4>
+          <p class="description" aria-label="Card long description">${this._data.description}</p>
         </div>
-      </a>
+      </div>
     `
+
+    this.setAttribute('role', 'listitem')
+    this.setAttribute('id', `${this._data.id}`)
+    this.setAttribute('tabindex', '0')
   }
 }
 
