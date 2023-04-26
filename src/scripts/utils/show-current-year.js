b05d01e4342d9bdbe13eee3a6b592bd7019f3147
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-26 18:32:03 +0700

    Fix show-current-year.js wrong logic

--- src/scripts/utils/show-current-year.js
+++ src/scripts/utils/show-current-year.js
@@ -2,14 +2,11 @@ const ShowCurrentYear = {
   init ({
     containerId
   }) {
-    if (typeof containerId === 'string') {
-      this._container = containerId
-    } else {
-      console.error('Inputted value is not a string!\nPlease provide a string of HTML id')
-    }
+    this._container = containerId
+    return this
   },
   show () {
-    document.getElementById(this._container).innerHTML = `${+new Date().getFullYear()}`
+    this._container.innerHTML = `${+new Date().getFullYear()}`
   }
 }
 
