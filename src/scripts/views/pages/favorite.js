ca9f530d9d7f673265db7e78ddb3b90c0433b4cd
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-26 20:12:24 +0700

    Update index.js, favorite.js

--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,8 +1,11 @@
 const Favorite = {
   async render () {
     return `
+      <h1>DishPlate</h1>
+      <br>
       <h2>Favorit</h2>
       <hr>
+      <div id="list" role="list"></div>
     `
   },
   async afterRender () {
