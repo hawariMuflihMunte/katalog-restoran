c034a4aac7ef79633c303ecc9a28fca93e4ddda5
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-26 16:26:11 +0700

    Create show-current-year.js

---
+++ src/scripts/utils/show-current-year.js
@@ -0,0 +1,16 @@
+const ShowCurrentYear = {
+  init ({
+    containerId
+  }) {
+    if (typeof containerId === 'string') {
+      this._container = containerId
+    } else {
+      console.error('Inputted value is not a string!\nPlease provide a string of HTML id')
+    }
+  },
+  show () {
+    document.getElementById(`${this._container}`).innerHTML = `${+new Date().getFullYear()}`
+  }
+}
+
+export default ShowCurrentYear
