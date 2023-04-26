e095cff9de1c389e59839b8214a8c525abeef92d
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-27 01:04:34 +0700

    List resources to be cached

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,3 +1,25 @@
+import CacheHelper from "./utils/cache-helper"
+
+const assetsToCache = [
+  './',
+  './icons/apple-touch-icon.png',
+  './icons/icon-48x48.png',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
+  './icons/icon-192x192.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  './favicon-16x16.png',
+  './favicon-32x32.png',
+  './index.html',
+  './app.bundle.js',
+  './manifestw.json',
+  './sw.bundle.js'
+]
+
 self.addEventListener('install', (event) => {
   console.log('installing Service Worker')
 
