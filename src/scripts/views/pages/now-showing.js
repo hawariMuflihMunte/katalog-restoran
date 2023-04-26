8117d461ac4414f4dcb03a3dc95f1d402186dd26
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-26 20:11:59 +0700

    Update selector

--- src/scripts/views/pages/now-showing.js
+++ src/scripts/views/pages/now-showing.js
@@ -1,12 +1,23 @@
+import DATA from '../../../data/DATA.json'
+import '../../components/card'
+
 const NowShowing = {
   async render () {
     return `
+      <h1>DishPlate</h1>
+      <br>
       <h2>Katalog</h2>
       <hr>
+      <div id="list" role="list"></div>
     `
   },
   async afterRender () {
-    // Logic upcoming
+    DATA.restaurants.forEach(data => {
+      const card = document.createElement('card-component')
+      card.data = data
+
+      document.getElementById('list').appendChild(card)
+    })
   }
 }
 
