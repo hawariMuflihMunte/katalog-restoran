e7b3ff7ed8142087bddb4e45fd77def91bc13bfd
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-26 17:01:21 +0700

    Update favorite.js, now-showing.js

---
+++ src/scripts/views/pages/favorite.js
@@ -0,0 +1,13 @@
+const Favorite = {
+  async render () {
+    return `
+      <h2>Favorit</h2>
+      <hr>
+    `
+  },
+  async afterRender () {
+    // Logic upcoming
+  }
+}
+
+export default Favorite
---
+++ src/scripts/views/pages/now-showing.js
@@ -0,0 +1,13 @@
+const NowShowing = {
+  async render () {
+    return `
+      <h2>Katalog</h2>
+      <hr>
+    `
+  },
+  async afterRender () {
+    // Logic upcoming
+  }
+}
+
+export default NowShowing
