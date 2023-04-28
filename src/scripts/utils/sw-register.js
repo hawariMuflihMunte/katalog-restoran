3a1c8012e9180ed542a8c1b7faec6ddaaf27e08a
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-28 07:28:40 +0700

    Delete workbox implementation in sw-register.js

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,15 +1,11 @@
-import * as WorboxWindow from 'workbox-window'
-
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
     console.log('Fitur ServiceWorker tidak didukung di web browser ini')
     return false
   }
 
-  const wb = new WorboxWindow.Workbox('./sw.bundle.js')
-
   try {
-    await wb.register()
+    await navigator.serviceWorker.register('./sw.bundle.js')
     console.log('Service Worker registered')
   } catch (error) {
     console.log('Failed to register service worker', error)
