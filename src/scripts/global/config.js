8261a0d1358138210813b5c570c53de82d1475ef
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-29 11:25:14 +0700

    Add BASE_API_IMAGE_URL prop

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,6 +1,7 @@
 const CONFIG = {
   CACHE_NAME: 'DishPlate-V1',
-  BASE_API_URL: 'https://restaurant-api.dicoding.dev/'
+  BASE_API_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_API_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/'
 }
 
 export default CONFIG
