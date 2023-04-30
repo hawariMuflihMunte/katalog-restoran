716d1c12143bd9d1ded90c582559a5c1678609f4
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-30 13:04:45 +0700

    Add global config props

--- src/scripts/global/config.js
+++ src/scripts/global/config.js
@@ -1,7 +1,9 @@
 const CONFIG = {
   CACHE_NAME: 'DishPlate-V1',
   BASE_API_URL: 'https://restaurant-api.dicoding.dev/',
-  BASE_API_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/'
+  BASE_API_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
+  DATABASE_NAME: 'dishplate-database',
+  OBJECT_STORE_NAME: 'dishplate'
 }
 
 export default CONFIG
