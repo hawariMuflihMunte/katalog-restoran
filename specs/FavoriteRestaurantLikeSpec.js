5d66c79225f3194366aa2383e7a7e12814fb4a98
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-19 17:51:57 +0700

    Change name to `FavoriteRestaurantLikeSpec.js`

---
+++ specs/FavoriteRestaurantLikeSpec.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+
+import FavoriteButtonInitiator from '../src/scripts/utils/favorite-button-initiator'
+
+describe('Liking restaurant', () => {
+  FavoriteButtonInitiator.init({
+
+  })
+
+  it('should display the favorite button', () => {
+    //
+  })
+})
