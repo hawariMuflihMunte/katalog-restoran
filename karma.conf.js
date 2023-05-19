ddcd3433da38f1e158804e60c52cfaa9808e11f0
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-19 15:28:44 +0700

    Init `karma`

--- karma.conf.js
+++ karma.conf.js
@@ -1,73 +1,68 @@
 // Karma configuration
-// Generated on Fri Jul 03 2020 20:15:52 GMT+0700 (Western Indonesia Time)
-module.exports = function (config) {
+// Generated on Fri May 19 2023 15:28:34 GMT+0700 (Western Indonesia Time)
+
+module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
+
     // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
     frameworks: ['jasmine'],
+
 
     // list of files / patterns to load in the browser
     files: [
-      'specs/**/*Spec.js'
     ],
 
+
     // list of files / patterns to exclude
-    exclude: [],
+    exclude: [
+    ],
+
 
     // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    // available preprocessors: https://www.npmjs.com/search?q=keywords:karma-preprocessor
     preprocessors: {
-      'specs/**/*Spec.js': ['webpack', 'sourcemap']
     },
 
-    webpack: {
-      // karma watches the test entry points
-      // (you don't need to specify the entry option)
-      // webpack watches dependencies
-      // webpack configuration
-      devtool: 'inline-source-map',
-      mode: 'development'
-    },
-
-    webpackMiddleware: {
-      // webpack-dev-middleware configuration
-      // i. e.
-      stats: 'errors-only'
-    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    // available reporters: https://www.npmjs.com/search?q=keywords:karma-reporter
     reporters: ['progress'],
+
 
     // web server port
     port: 9876,
 
+
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
+
     // level of logging
-    /* possible values: config.LOG_DISABLE || config.LOG_ERROR
-    || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG */
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
+
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
+
     // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    // available browser launchers: https://www.npmjs.com/search?q=keywords:karma-launcher
+    browsers: ['Chrome', 'ChromeHeadless'],
+
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
 
     // Concurrency level
-    // how many browser should be started simultaneous
+    // how many browser instances should be started simultaneously
     concurrency: Infinity
   })
 }
