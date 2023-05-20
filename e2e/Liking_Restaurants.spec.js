80019ed5260ba182724a9a5407506a7d0efd5c4f
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-20 14:51:50 +0700

    E2E Test Suites + Liking restaurant, and + Unliking restaurant

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,6 +1,23 @@
 /* eslint-disable no-undef */
 Feature('Liking Restaurants')
 
-Scenario('test something', ({ I }) => {
+Before(({ I }) => {
+  I.amOnPage('/#/favorite')
+})
 
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.see('There are currently no restaurant(s) in your favorites list.', 'div#list')
+})
+
+Scenario('liking one restaurant', ({ I }) => {
+  I.amOnPage('/#/')
+  I.waitForElement(locate('.card').first(), 5)
+  I.seeElement(locate('.card-title a').first())
+  I.click(locate('.card-title a').first())
+
+  I.wait(10)
+
+  I.seeElement(locate('.add-to-favorite'))
+  I.click(locate('.add-to-favorite'))
+  I.see('💔', locate('.add-to-favorite'))
 })
--- e2e/Unliking_Restaurants.spec.js
+++ e2e/Unliking_Restaurants.spec.js
@@ -1,6 +1,41 @@
 /* eslint-disable no-undef */
 Feature('Unliking Restaurants')
 
-Scenario('test something', ({ I }) => {
+Before(({ I }) => {
+  I.amOnPage('/#/favorite')
+})
 
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.see('There are currently no restaurant(s) in your favorites list.', 'div#list')
+})
+
+Scenario('unliking one restaurant', ({ I }) => {
+  // Liking
+  I.amOnPage('/#/')
+  I.waitForElement(locate('.card').first(), 5)
+  I.seeElement(locate('.card-title a').first())
+  I.click(locate('.card-title a').first())
+
+  I.wait(10)
+
+  I.seeElement(locate('.add-to-favorite'))
+  I.click(locate('.add-to-favorite'))
+  I.see('💔', '.add-to-favorite')
+
+  I.seeElement('.home-button')
+  I.click(locate('.home-button'))
+
+  // Unliking
+  I.amOnPage('/#/')
+  I.waitForElement(locate('.card').first(), 10)
+  I.seeElement(locate('.card-title a').first())
+  I.click(locate('.card-title a').first())
+
+  I.wait(10)
+
+  I.seeElement(locate('.add-to-favorite'))
+  I.see('💔', '.add-to-favorite')
+
+  I.click(locate('.add-to-favorite'))
+  I.see('🧡', '.add-to-favorite')
 })
