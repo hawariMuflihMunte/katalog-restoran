8ed90846e6687d26505682f20c8c54bdd1cf70d3
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-20 18:47:38 +0700

    Use `BundleAnalyzerPlugin` in `dev` stage

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,19 +2,24 @@ const { merge } = require('webpack-merge')
 const path = require('path')
 const common = require('./webpack.common')
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    open: true,
-    port: 9000,
-    client: {
-      overlay: {
-        errors: true,
-        warnings: true
-      }
-    },
-    compress: true
-  }
+  // devServer: {
+  //   static: path.resolve(__dirname, 'dist'),
+  //   open: true,
+  //   port: 9000,
+  //   client: {
+  //     overlay: {
+  //       errors: true,
+  //       warnings: true
+  //     }
+  //   },
+  //   compress: true
+  // }
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ]
 })
