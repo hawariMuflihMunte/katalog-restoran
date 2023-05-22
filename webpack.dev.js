1fc8811d9254d7885a798171cc9e5d6181b1eab1
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-05-22 09:58:21 +0700

    Delete BundleAnalyzer import statement

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,8 +3,6 @@ const { merge } = require('webpack-merge')
 const path = require('path')
 const common = require('./webpack.common')
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
